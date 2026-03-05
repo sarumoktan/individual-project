20627a9fefea793dce293edcb4132797cce48279
sarumoktan <sarumoktan198@gmail.com>
2026-03-05 07:21:49 +0545

    refactor: update utility functions and city constants

---
+++ client/src/utils/cities.js
@@ -0,0 +1,24 @@
+const nepalCities = [
+  "Kathmandu",
+  "Pokhara",
+  "Chitwan",
+  "Lumbini",
+  "Biratnagar",
+  "Birgunj",
+  "Dharan",
+  "Janakpur",
+  "Butwal",
+  "Nepalgunj",
+  "Dhangadhi",
+  "Hetauda",
+  "Narayanghat",
+  "Bhairahawa",
+  "Itahari",
+  "Mugling",
+  "Dumre",
+  "Damauli",
+  "Gorkha",
+  "Baglung",
+];
+
+export default nepalCities;
