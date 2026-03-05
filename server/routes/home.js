b97bc538907f228a829f3f0fdd64e6e83455e6d1
sarumoktan <sarumoktan198@gmail.com>
2026-03-05 07:32:51 +0545

    remove old server controllers, routes, and vercel config

--- server/routes/home.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Manage your bus bookings and travel plans" });
-});
-
-export default router;
