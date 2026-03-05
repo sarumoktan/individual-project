774b57393cd17f07efa7b7828071541fa9e047dc
sarumoktan <sarumoktan198@gmail.com>
2026-03-05 07:21:48 +0545

    feat: implement centralized middleware for auth and error handling

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "token missing",
+      token: authHeader,
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: `token expired`,
+      error: error.message,
+    });
+  }
+};
+
+module.exports = authMiddleware;
+
